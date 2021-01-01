941d104645397a468eebbafdd86bed2b267de623
16119 <dangtruongsinh123@gmail.com>
2021-01-01 22:40:39 +0700

    first commit

---
+++ src/main/java/com/example/springmvcjenkins/SpringmvcjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcjenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmvcjenkins/SpringmvcjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
